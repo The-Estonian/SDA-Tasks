811d1d1afcf02963e78a9d1aa00ee1f35e58d3d9
The-Estonian <Zaar2213@gmail.com>
2021-10-30 19:45:21 +0300

    Task3.py task done commit

--- Task3.py
+++ Task3.py
@@ -1,19 +1,15 @@
 try:
     income = float(input())
-    if income <= 8552800:
-        income = (income - 556.02) / 100 * 18
-        print(income)
+    if income <= 85528.00:
+        income = (income / 100 * 18) - 556.02
+        if income < 0:
+            income = 0
+            print(income)
+        else:
+            print(income)
     else:
-        income = (income - 1483902) / 100 * 32 + 1483902
+        income = (income - 85528.00) / 100 * 32 + 14839.02
         print(income)
 
-
-
-
-
-
-
-
-
-except ValueError:
-    print("Bad bad value")
+except:
+    print("Please enter the value in numbers.")
