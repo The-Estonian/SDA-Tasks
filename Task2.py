9877075c7ca43ed3dc07070652bad659b682f589
The-Estonian <Zaar2213@gmail.com>
2021-10-30 18:13:23 +0300

    Task2.py task done commit

--- Task2.py
+++ Task2.py
@@ -1,6 +1,6 @@
 try:
-    temp_in_Celsius = float(input("Please enter Temperature: "))
+    temp_in_Celsius = float(input())
     print(1.8 * temp_in_Celsius + 32)
     
 except ValueError:
-    print("Please enter a valid temperature number in Celsius")
+    print("Please enter a correct number.")
