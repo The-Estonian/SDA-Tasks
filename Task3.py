6b23907f82651478bee9f36d52114dcd127367ef
The-Estonian <Zaar2213@gmail.com>
2021-10-30 18:40:51 +0300

    Task3.py middleway.

--- Task3.py
+++ Task3.py
@@ -1,5 +1,12 @@
 try:
     income = float(input())
+    if income <= 8552800:
+        income = (income - 556.02) / 100 * 18
+        print(income)
+    else:
+        income = (income - 1483902) / 100 * 32 + 1483902
+        print(income)
+
 
 
 
