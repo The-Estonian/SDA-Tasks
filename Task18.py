3f6524e18e81ba99cafc8b0fba9ca9bc3c32a860
The-Estonian <Zaar2213@gmail.com>
2021-11-04 19:04:09 +0200

    Task18.py half working test

--- Task18.py
+++ Task18.py
@@ -7,44 +7,62 @@ Example b: text: alice has a cat, n: 26, result: alice has a cat
 Get data from the user in the console using argument-less input()."""
 
 #sentence = str(input("Please enter sentence to be encypted: "))
-from typing import ValuesView
+while True:
 
+    sentence_back = ""
+    reconstruct = []
+    reconstruct_key = []
+    dissiminate = []
+    #sentence = str(input())
+    sentence = "this is a sentence"
+    n = 13
+    alphabet = {
+                1:"a", 
+                2:"b", 
+                3:"c", 
+                4:"d", 
+                5:"e", 
+                6:"f", 
+                7:"g", 
+                8:"h",
+                9:"i", 
+                10:"j", 
+                11:"k", 
+                12:"l", 
+                13:"m", 
+                14:"n", 
+                15:"o",
+                16:"p", 
+                17:"q", 
+                18:"r", 
+                19:"s", 
+                20:"t", 
+                21:"u", 
+                22:"v", 
+                23:"w", 
+                24:"x", 
+                25:"y", 
+                26:"z",
+                27:" "}
+# Iterating over each letter in sentence and applying letter key value to dissiminate list.
+    for _ in sentence:
+        for key, value in alphabet.items():
+            if value == _:
+                dissiminate.append(key)
+# Iterating over each key value in dissiminate list and applying "n" times value to key. Also ignoring "space" key value in process.
+    for _ in dissiminate:
+        if _ == 27:
+            reconstruct_key.append(_)
+        else:
+            reconstruct_key.append(_+n)
+# Iterating over alphabet dictionary with new key values to find new letters and appending them into reconstruct list.
+    for _ in reconstruct_key:
+        for key, value in alphabet.items():
+            if key == _:
+                reconstruct.append(value)
+# Iterating over reconstruct list and assimilating them to new sentences.
+    for _ in reconstruct:
+        sentence_back += _
 
-reconstruct = []
-dissiminate = []
-sentence = "randomness"
-n = 5
-alphabet = {
-            1:"a", 
-            2:"b", 
-            3:"c", 
-            4:"d", 
-            5:"e", 
-            6:"f", 
-            7:"g", 
-            8:"h",
-            9:"i", 
-            10:"j", 
-            11:"k", 
-            12:"l", 
-            13:"m", 
-            14:"n", 
-            15:"o",
-            16:"p", 
-            17:"q", 
-            18:"r", 
-            19:"s", 
-            20:"t", 
-            21:"u", 
-            22:"v", 
-            23:"w", 
-            24:"x", 
-            25:"y", 
-            26:"z"}
-
-for _ in sentence:
-    for key, value in alphabet.items():
-        if value == _:
-            dissiminate.append(key)
-print(dissiminate)
-
+    print(sentence_back)
+    break
