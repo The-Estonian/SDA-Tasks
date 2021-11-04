ecb384792e20019c4deabf86ea610137f712cd39
The-Estonian <Zaar2213@gmail.com>
2021-11-04 13:46:26 +0200

    Task17.py task done commit

--- Task17.py
+++ Task17.py
@@ -11,27 +11,30 @@ Example c: ") 2 * 4 (", result: Incorrect pairing of brackets
 Get data from the user in the console using argument-less input()."""
 
 while True:
-    # input = input()
+    #input = str(input())
+    input = "((2 * 4)"
     #input = "2 * (3.4 - (-7) / 2) * (a-2) / (b-1)))"
-    input = "(2 * (3.4 - (-7) / 2) * (a-2) / (b-1))"
+    #input = "(2 * (3.4 - (-7) / 2) * (a-2) / (b-1))"
     #input = "Today i saw ) and then (" + str(2 + 0)
     #input = ")This is not the correct way of doing things("
+
     left = 0
     right = 0
-    
+    true = 0
+
     for i in input:
-        right = input.count(")")
+        if i == ")":
+            right += 1
+            if right > left:
+                true += 1
+        if i == "(":
+            left += 1
+    if true > 0:
+        print("Incorrect pairing of brackets")
+    elif left != right:
+        print("Incorrect pairing of brackets")
+    elif left == right:
+        print("OK")
 
-        if right > left:
-            print("Incorrect pairing of brackets BREAK")
-            print(left, right)
-            break
-
-        left = input.count("(")
-        if left == right:
-            print("OK")
-        if left != right:
-            print("Incorrect pairing of brackets")
-
-        print(left, right)
-        
+    print(left, right)
+    break
