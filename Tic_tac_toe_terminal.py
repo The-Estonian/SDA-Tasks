6cc078ff61bf459f1404c14cd42c3b2a4bfab62f
The-Estonian <Zaar2213@gmail.com>
2021-11-08 17:37:19 +0200

    Game box class and usability done

--- Tic_tac_toe_terminal.py
+++ Tic_tac_toe_terminal.py
@@ -80,18 +80,36 @@ a2 = "│      │"
 a3 = "│      │"
 a4 = "╰──────╯" 
 
-# Class?
+# Class Box() to fill the grid based on answers
 class Box():
+   """
+   Class Box(integer, string) takes 2 variables:
+   integer with value 1-9 to choose a box from the grid
+   string with value X or O to insert the value to choosen box
+   
+   """
+
    def __init__(self, box_num, player):
       self.player = player
       self.box_num = box_num
       if self.box_num == 1:
-         box_1()
+         self.box_1()
       if self.box_num == 2:
-         box_2()
-
-
-
+         self.box_2()
+      if self.box_num == 3:
+         self.box_3()
+      if self.box_num == 4:
+         self.box_4()
+      if self.box_num == 5:
+         self.box_5()
+      if self.box_num == 6:
+         self.box_6()
+      if self.box_num == 7:
+         self.box_7()
+      if self.box_num == 8:
+         self.box_8()
+      if self.box_num == 9:
+         self.box_9()
 
    # 1st box function to switch value to O or X
    def box_1(self):
@@ -108,112 +126,112 @@ class Box():
          f = f[:2] + b4[0:8] + f[10:]
 
    # 2nd box function to switch value to O or X
-   def box_2(var_var2):
+   def box_2(self):
       global b, c, d, f
-      if var_var2 == "O":
+      if self.player == "O":
          b = b[:13] + a1[:8] + b[21:]
          c = c[:13] + a2[:8] + c[21:]
          d = d[:13] + a3[:8] + d[21:]
          f = f[:13] + a4[:8] + f[21:]
-      if var_var2 == "X":
+      if self.player == "X":
          b = b[:13] + b1[:8] + b[21:]
          c = c[:13] + b2[:8] + c[21:]
          d = d[:13] + b3[:8] + d[21:]
          f = f[:13] + b4[:8] + f[21:]
 
    # 3rd box function to switch value to O or X
-   def box_3(var_var3):
+   def box_3(self):
       global b, c, d, f
-      if var_var3 == "O":
+      if self.player == "O":
          b = b[:24] + a1[:8] + b[32:]
          c = c[:24] + a2[:8] + c[32:]
          d = d[:24] + a3[:8] + d[32:]
          f = f[:24] + a4[:8] + f[32:]
-      if var_var3 == "X":
+      if self.player == "X":
          b = b[:24] + b1[:8] + b[32:]
          c = c[:24] + b2[:8] + c[32:]
          d = d[:24] + b3[:8] + d[32:]
          f = f[:24] + b4[:8] + f[32:]
 
    # 4th box function to switch value to O or X
-   def box_4(var_var4):
+   def box_4(self):
       global h, i, j, k
-      if var_var4 == "O":
+      if self.player == "O":
          h = h[:2] + a1[0:8] + h[10:]
          i = i[:2] + a2[0:8] + i[10:]
          j = j[:2] + a3[0:8] + j[10:]
          k = k[:2] + a4[0:8] + k[10:]
-      if var_var4 == "X":
+      if self.player == "X":
          h = h[:2] + b1[0:8] + h[10:]
          i = i[:2] + b2[0:8] + i[10:]
          j = j[:2] + b3[0:8] + j[10:]
          k = k[:2] + b4[0:8] + k[10:]
 
    # 5th box function to switch value to O or X
-   def box_5(var_var5):
+   def box_5(self):
       global h, i, j, k
-      if var_var5 == "O":
+      if self.player == "O":
          h = h[:13] + a1[:8] + h[21:]
          i = i[:13] + a2[:8] + i[21:]
          j = j[:13] + a3[:8] + j[21:]
          k = k[:13] + a4[:8] + k[21:]
-      if var_var5 == "X":
+      if self.player == "X":
          h = h[:13] + b1[:8] + h[21:]
          i = i[:13] + b2[:8] + i[21:]
          j = j[:13] + b3[:8] + j[21:]
          k = k[:13] + b4[:8] + k[21:]
 
    # 6th box function to switch value to O or X
-   def box_6(var_var6):
+   def box_6(self):
       global h, i, j, k
-      if var_var6 == "O":
+      if self.player == "O":
          h = h[:24] + a1[:8] + h[32:]
          i = i[:24] + a2[:8] + i[32:]
          j = j[:24] + a3[:8] + j[32:]
          k = k[:24] + a4[:8] + k[32:]
-      if var_var6 == "X":
+      if self.player == "X":
          h = h[:24] + b1[:8] + h[32:]
          i = i[:24] + b2[:8] + i[32:]
          j = j[:24] + b3[:8] + j[32:]
          k = k[:24] + b4[:8] + k[32:]
 
    # 7th box function to switch value to O or X
-   def box_7(var_var7):
+   def box_7(self):
       global n, p, q, r
-      if var_var7 == "O":
+      if self.player == "O":
          n = n[:2] + a1[0:8] + n[10:]
          p = p[:2] + a2[0:8] + p[10:]
          q = q[:2] + a3[0:8] + q[10:]
          r = r[:2] + a4[0:8] + r[10:]
-      if var_var7 == "X":
+      if self.player == "X":
          n = n[:2] + b1[0:8] + n[10:]
          p = p[:2] + b2[0:8] + p[10:]
          q = q[:2] + b3[0:8] + q[10:]
          r = r[:2] + b4[0:8] + r[10:]
 
    # 8th box function to switch value to O or X
-   def box_8(var_var8):
+   def box_8(self):
       global n, p, q, r
-      if var_var8 == "O":
+      if self.player == "O":
          n = n[:13] + a1[:8] + n[21:]
          p = p[:13] + a2[:8] + p[21:]
          q = q[:13] + a3[:8] + q[21:]
          r = r[:13] + a4[:8] + r[21:]
-      if var_var8 == "X":
+      if self.player == "X":
          n = h[:13] + b1[:8] + n[21:]
          p = i[:13] + b2[:8] + p[21:]
          q = j[:13] + b3[:8] + q[21:]
          r = k[:13] + b4[:8] + r[21:]
 
    # 9th box function to switch value to O or X
-   def box_9(var_var9):
+   def box_9(self):
       global n, p, q, r
-      if var_var9 == "O":
+      if self.player == "O":
          n = n[:24] + a1[:8] + n[32:]
          p = p[:24] + a2[:8] + p[32:]
          q = q[:24] + a3[:8] + q[32:]
          r = r[:24] + a4[:8] + r[32:]
-      if var_var9 == "X":
+      if self.player == "X":
          n = n[:24] + b1[:8] + n[32:]
          p = p[:24] + b2[:8] + p[32:]
          q = q[:24] + b3[:8] + q[32:]
@@ -248,15 +266,20 @@ while True:
    list_x = []
    list_o = []
 
-   for _ in range(5):
+   while len(list_x) + len(list_o) <= 8:
       answer_x = int(input("Player X please choose a box to occupy: "))
       list_x.append(answer_x)
       Box(answer_x, "X")
 
-   for _ in range(4):
+      os.system("cls")
+      print(f"{a}\n{b}\n{c}\n{d}\n{f}\n{g}\n{h}\n{i}\n{j}\n{k}\n{m}\n{n}\n{p}\n{q}\n{r}\n{s}")
+
       answer_o = int(input("Player O please choose a box to occupy: "))
       list_o.append(answer_o)
       Box(answer_o, "O")
+
+      os.system("cls")
+      print(f"{a}\n{b}\n{c}\n{d}\n{f}\n{g}\n{h}\n{i}\n{j}\n{k}\n{m}\n{n}\n{p}\n{q}\n{r}\n{s}")
 
 
 
