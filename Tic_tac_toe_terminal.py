0ad63621a501eb4bf85c92812fa8f23661dccfcd
The-Estonian <Zaar2213@gmail.com>
2021-11-08 13:10:53 +0200

    Game Box5 and Box6 variables done

--- Tic_tac_toe_terminal.py
+++ Tic_tac_toe_terminal.py
@@ -132,10 +132,53 @@ def box_3(var_var3):
       d = d[:24] + a3[:8] + d[32:]
       f = f[:24] + a4[:8] + f[32:]
    if var_var3 == "X":
-      b = b[:3] + b1[:8] + b[10:]
-      c = c[:3] + b2[:8] + c[10:]
-      d = d[:3] + b3[:8] + d[10:]
-      f = f[:3] + b4[:8] + f[10:]
+      b = b[:24] + b1[:8] + b[32:]
+      c = c[:24] + b2[:8] + c[32:]
+      d = d[:24] + b3[:8] + d[32:]
+      f = f[:24] + b4[:8] + f[32:]
+
+# 4th box function to switch value to O or X
+def box_4(var_var4):
+   global h, i, j, k
+   if var_var4 == "O":
+      h = h[:2] + a1[0:8] + h[10:]
+      i = i[:2] + a2[0:8] + i[10:]
+      j = j[:2] + a3[0:8] + j[10:]
+      k = k[:2] + a4[0:8] + k[10:]
+   if var_var4 == "X":
+      h = h[:2] + b1[0:8] + h[10:]
+      i = i[:2] + b2[0:8] + i[10:]
+      j = j[:2] + b3[0:8] + j[10:]
+      k = k[:2] + b4[0:8] + k[10:]
+
+# 5th box function to switch value to O or X
+def box_5(var_var5):
+   global h, i, j, k
+   if var_var5 == "O":
+      h = h[:13] + a1[:8] + h[21:]
+      i = i[:13] + a2[:8] + i[21:]
+      j = j[:13] + a3[:8] + j[21:]
+      k = k[:13] + a4[:8] + k[21:]
+   if var_var5 == "X":
+      h = h[:13] + b1[:8] + h[21:]
+      i = i[:13] + b2[:8] + i[21:]
+      j = j[:13] + b3[:8] + j[21:]
+      k = k[:13] + b4[:8] + k[21:]
+
+# 6th box function to switch value to O or X
+def box_6(var_var6):
+   global h, i, j, k
+   if var_var6 == "O":
+      h = h[:24] + a1[:8] + h[32:]
+      i = i[:24] + a2[:8] + i[32:]
+      j = j[:24] + a3[:8] + j[32:]
+      k = k[:24] + a4[:8] + k[32:]
+   if var_var6 == "X":
+      h = h[:24] + b1[:8] + h[32:]
+      i = i[:24] + b2[:8] + i[32:]
+      j = j[:24] + b3[:8] + j[32:]
+      k = k[:24] + b4[:8] + k[32:]
+
       
 
 while True:
@@ -177,7 +220,33 @@ while True:
       elif var2 == ("O"):
          box_2("O")
 
+# Box3 variable activator
+      var3 = input("Box3: ")
+      if var3 == ("X"):
+         box_3("X")
+      elif var3 == ("O"):
+         box_3("O")
 
+# Box4 variable activator
+      var4 = input("Box4: ")
+      if var4 == ("X"):
+         box_4("X")
+      elif var4 == ("O"):
+         box_4("O")
+
+# Box5 variable activator
+      var5 = input("Box5: ")
+      if var5 == ("X"):
+         box_5("X")
+      elif var5 == ("O"):
+         box_5("O")
+
+# Box6 variable activator
+      var6 = input("Box6: ")
+      if var6 == ("X"):
+         box_6("X")
+      elif var6 == ("O"):
+         box_6("O")
 
 
 
