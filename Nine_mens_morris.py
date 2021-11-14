48b1fc5045114e0cb2f725745349a4ea55dc8bc4
The-Estonian <Zaar2213@gmail.com>
2021-11-14 23:41:38 +0200

    daily save

--- Nine_mens_morris.py
+++ Nine_mens_morris.py
@@ -111,15 +111,99 @@ w1= "└────────────────────────
 
 ##################################################################################################################
 
-def grid_move():
-    num = 0
-    for _ in range(61):
-        num += 1
-        os.system("cls")
-        print(f"{a3[:num]}\n{a2[:num]}\n{a1[:num]}\n{a[:num]}\n{b[:num]}\n{c[:num]}\n{d[:num]}\n{e[:num]}\
-            \n{f[:num]}\n{g[:num]}\n{h[:num]}\n{j[:num]}\n{k[:num]}\n{m[:num]}\n{n[:num]}\n{p[:num]}\
-            \n{q[:num]}\n{r[:num]}\n{s[:num]}\n{t[:num]}\n{u[:num]}\n{v[:num]}\n{w[:num]}\n{w1[:num]}")
-            
+class Grid:
+   def __init__(self, var):
+      self.grid_on()
+      self.grid_off()
+      if var == "both":
+         self.grid_on()
+         self.grid_off()
+      elif var == "on":
+         self.grid_on()
+      elif var == "off":
+         self.grid_off()
+
+   def grid_on():
+      os.system("cls")
+      grid_index = {
+         "key_a3" : 61,
+         "key_a2" : 66,
+         "key_a1" : 71,
+         "key_a"  : 76,
+         "key_b"  : 81,
+         "key_c"  : 86,
+         "key_d"  : 91,
+         "key_e"  : 96,
+         "key_f"  : 101,
+         "key_g"  : 106,
+         "key_h"  : 111,
+         "key_j"  : 116,
+         "key_k"  : 121,
+         "key_m"  : 126,
+         "key_n"  : 131,
+         "key_p"  : 136,
+         "key_q"  : 141,
+         "key_r"  : 146,
+         "key_s"  : 151,
+         "key_t"  : 156,
+         "key_u"  : 161,
+         "key_v"  : 166,
+         "key_w"  : 171,
+         "key_w1" : 176}
+      for _ in range(37):
+         for key, value in grid_index.items():
+               if value > 0:
+                  grid_index[key] -= 5
+               elif value <0:
+                  grid_index[key] = 0
+         os.system("cls")  
+         print(f"""{a3[grid_index["key_a3"]:]}\n{a2[grid_index["key_a2"]:]}\n{a1[grid_index["key_a1"]:]}\n{a[grid_index["key_a"]:]}\
+               \n{b[grid_index["key_b"]:]}\n{c[grid_index["key_c"]:]}\n{d[grid_index["key_d"]:]}\n{e[grid_index["key_e"]:]}\
+               \n{f[grid_index["key_f"]:]}\n{g[grid_index["key_g"]:]}\n{h[grid_index["key_h"]:]}\n{j[grid_index["key_j"]:]}\
+               \n{k[grid_index["key_k"]:]}\n{m[grid_index["key_m"]:]}\n{n[grid_index["key_n"]:]}\n{p[grid_index["key_p"]:]}\
+               \n{q[grid_index["key_q"]:]}\n{r[grid_index["key_r"]:]}\n{s[grid_index["key_s"]:]}\n{t[grid_index["key_t"]:]}\
+               \n{u[grid_index["key_u"]:]}\n{v[grid_index["key_v"]:]}\n{w[grid_index["key_w"]:]}\n{w1[grid_index["key_w1"]:]}""")
+
+   def grid_off():
+      os.system("cls")
+      grid_index = {
+         "key_a3" : 61,
+         "key_a2" : 66,
+         "key_a1" : 71,
+         "key_a"  : 76,
+         "key_b"  : 81,
+         "key_c"  : 86,
+         "key_d"  : 91,
+         "key_e"  : 96,
+         "key_f"  : 101,
+         "key_g"  : 106,
+         "key_h"  : 111,
+         "key_j"  : 116,
+         "key_k"  : 121,
+         "key_m"  : 126,
+         "key_n"  : 131,
+         "key_p"  : 136,
+         "key_q"  : 141,
+         "key_r"  : 146,
+         "key_s"  : 151,
+         "key_t"  : 156,
+         "key_u"  : 161,
+         "key_v"  : 166,
+         "key_w"  : 171,
+         "key_w1" : 176}
+      for _ in range(37):
+         for key, value in grid_index.items():
+               if value > 0:
+                  grid_index[key] += 5
+               elif value <0:
+                  grid_index[key] = 0
+         os.system("cls")  
+         print(f"""{a3[grid_index["key_a3"]:]}\n{a2[grid_index["key_a2"]:]}\n{a1[grid_index["key_a1"]:]}\n{a[grid_index["key_a"]:]}\
+               \n{b[grid_index["key_b"]:]}\n{c[grid_index["key_c"]:]}\n{d[grid_index["key_d"]:]}\n{e[grid_index["key_e"]:]}\
+               \n{f[grid_index["key_f"]:]}\n{g[grid_index["key_g"]:]}\n{h[grid_index["key_h"]:]}\n{j[grid_index["key_j"]:]}\
+               \n{k[grid_index["key_k"]:]}\n{m[grid_index["key_m"]:]}\n{n[grid_index["key_n"]:]}\n{p[grid_index["key_p"]:]}\
+               \n{q[grid_index["key_q"]:]}\n{r[grid_index["key_r"]:]}\n{s[grid_index["key_s"]:]}\n{t[grid_index["key_t"]:]}\
+               \n{u[grid_index["key_u"]:]}\n{v[grid_index["key_v"]:]}\n{w[grid_index["key_w"]:]}\n{w1[grid_index["key_w1"]:]}""")
 
 ##################################################################################################################
 
@@ -153,8 +237,8 @@ while True:
       os.system("cls")
       break
    elif game_mode == "1":
-      grid_refresh()
-      time.sleep(3)
+      Grid("on")
+      time.sleep(1)
    elif game_mode == "3":
       os.system("cls")
       print(rules)
@@ -163,8 +247,3 @@ while True:
          continue
       else:
          continue
-# def fresh_grid():
-#    os.system("cls")
-#    print(f"{a3}\n{a2}\n{a1}\n{a}\n{b}\n{c}\n{d}\n{f}\n{e}\n{g}\n{h}\n{j}\n{k}\n{m}\n{n}\n{p}\n{q}\n{r}\n{s}\n{t}\n{u}\n{v}")
-
-# fresh_grid()
--- supergrid.py
+++ supergrid.py
@@ -29,58 +29,50 @@ u = "│   ┌─┴─┐                   ┌─┴─┐                   �
 v = "│   │ 22├───────────────────┤ 23├───────────────────┤ 24│   │"
 w = "│   └───┘                   └───┘                   └───┘   │"
 w1= "└───────────────────────────────────────────────────────────┘"
-
+#    3:61
 ##################################################################################################################
 
 def grid_move():
     os.system("cls")
     grid_index = {
-        "key_a3" : -61,
-        "key_a2" : -63,
-        "key_a1" : -66,
-        "key_a"  : -69,
-        "key_b"  : -72,
-        "key_c"  : -75,
-        "key_d"  : -78,
-        "key_e"  : -81,
-        "key_f"  : -84,
-        "key_g"  : -87,
-        "key_h"  : -90,
-        "key_j"  : -93,
-        "key_k"  : -96,
-        "key_m"  : -99,
-        "key_n"  : -102,
-        "key_p"  : -105,
-        "key_q"  : -108,
-        "key_r"  : -111,
-        "key_s"  : -114,
-        "key_t"  : -117,
-        "key_u"  : -120,
-        "key_v"  : -123,
-        "key_w"  : -126,
-        "key_w1" : -129}
+        "key_a3" : 61,
+        "key_a2" : 66,
+        "key_a1" : 71,
+        "key_a"  : 76,
+        "key_b"  : 81,
+        "key_c"  : 86,
+        "key_d"  : 91,
+        "key_e"  : 96,
+        "key_f"  : 101,
+        "key_g"  : 106,
+        "key_h"  : 111,
+        "key_j"  : 116,
+        "key_k"  : 121,
+        "key_m"  : 126,
+        "key_n"  : 131,
+        "key_p"  : 136,
+        "key_q"  : 141,
+        "key_r"  : 146,
+        "key_s"  : 151,
+        "key_t"  : 156,
+        "key_u"  : 161,
+        "key_v"  : 166,
+        "key_w"  : 171,
+        "key_w1" : 176}
 
-    print(f"""{a3[:grid_index["key_a3"]]}\n{a2[:grid_index["key_a2"]]}\n{a1[:grid_index["key_a1"]]}\n{a[:grid_index["key_a"]]}\
-            \n{b[:grid_index["key_b"]]}\n{c[:grid_index["key_c"]]}\n{d[:grid_index["key_d"]]}\n{e[:grid_index["key_e"]]}\
-            \n{f[:grid_index["key_f"]]}\n{g[:grid_index["key_g"]]}\n{h[:grid_index["key_h"]]}\n{j[:grid_index["key_j"]]}\
-            \n{k[:grid_index["key_k"]]}\n{m[:grid_index["key_m"]]}\n{n[:grid_index["key_n"]]}\n{p[:grid_index["key_p"]]}\
-            \n{q[:grid_index["key_q"]]}\n{r[:grid_index["key_r"]]}\n{s[:grid_index["key_s"]]}\n{t[:grid_index["key_t"]]}\
-            \n{u[:grid_index["key_u"]]}\n{v[:grid_index["key_v"]]}\n{w[:grid_index["key_w"]]}\n{w1[:grid_index["key_w1"]]}""")
-
-    for _ in range(130):
+    for _ in range(37):
         for key, value in grid_index.items():
-            if value < 61:
-                grid_index[key] += 1
-            elif value == 61:
-                pass
-            
-        print(f"""{a3[:grid_index["key_a3"]]}\n{a2[:grid_index["key_a2"]]}\n{a1[:grid_index["key_a1"]]}\n{a[:grid_index["key_a"]]}\
-            \n{b[:grid_index["key_b"]]}\n{c[:grid_index["key_c"]]}\n{d[:grid_index["key_d"]]}\n{e[:grid_index["key_e"]]}\
-            \n{f[:grid_index["key_f"]]}\n{g[:grid_index["key_g"]]}\n{h[:grid_index["key_h"]]}\n{j[:grid_index["key_j"]]}\
-            \n{k[:grid_index["key_k"]]}\n{m[:grid_index["key_m"]]}\n{n[:grid_index["key_n"]]}\n{p[:grid_index["key_p"]]}\
-            \n{q[:grid_index["key_q"]]}\n{r[:grid_index["key_r"]]}\n{s[:grid_index["key_s"]]}\n{t[:grid_index["key_t"]]}\
-            \n{u[:grid_index["key_u"]]}\n{v[:grid_index["key_v"]]}\n{w[:grid_index["key_w"]]}\n{w1[:grid_index["key_w1"]]}""")
-        time.sleep(0.05)
-        os.system("cls")
+            if value > 0:
+                grid_index[key] -= 5
+            elif value <0:
+                grid_index[key] = 0
+        os.system("cls")  
+        print(f"""{a3[grid_index["key_a3"]:]}\n{a2[grid_index["key_a2"]:]}\n{a1[grid_index["key_a1"]:]}\n{a[grid_index["key_a"]:]}\
+            \n{b[grid_index["key_b"]:]}\n{c[grid_index["key_c"]:]}\n{d[grid_index["key_d"]:]}\n{e[grid_index["key_e"]:]}\
+            \n{f[grid_index["key_f"]:]}\n{g[grid_index["key_g"]:]}\n{h[grid_index["key_h"]:]}\n{j[grid_index["key_j"]:]}\
+            \n{k[grid_index["key_k"]:]}\n{m[grid_index["key_m"]:]}\n{n[grid_index["key_n"]:]}\n{p[grid_index["key_p"]:]}\
+            \n{q[grid_index["key_q"]:]}\n{r[grid_index["key_r"]:]}\n{s[grid_index["key_s"]:]}\n{t[grid_index["key_t"]:]}\
+            \n{u[grid_index["key_u"]:]}\n{v[grid_index["key_v"]:]}\n{w[grid_index["key_w"]:]}\n{w1[grid_index["key_w1"]:]}""")
+        
         
 grid_move()
